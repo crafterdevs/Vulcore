1bd53354a2fc9f92a368dc61bcf3213e4659e694
abdullah <abdullah.balikci20@ogr.atauni.edu.tr>
2025-05-02 15:57:05 +0300

    feat: update

---
+++ Vulcore/VulCore.Example/Program.cs
@@ -0,0 +1,10 @@
+﻿using Vulcore.Entities;
+
+Console.WriteLine("Hello, World!");
+
+var product = new Product
+{
+    Name = "1",
+    Description = "asd",
+    Count = 1
+};
--- Vulcore/Vulcore.Domain/Entities/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Vulcore.Domain.Entities;
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
